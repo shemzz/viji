10b29003d5b761124303f8141341f9227e77be77
Shemang David <davidshemang@gmail.com>
2023-10-22 17:03:59 +0100

    update to node api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4343/api/v1/',
-    apiHome: 'http://localhost:4343',
+    apiUrl: 'http://54.234.160.77:4343/api/v1/',
+    apiHome: 'http://54.234.160.77:4343',
     httpOptions: {
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
