7f202e6fb1d4361e9ef42692089acb35237eab70
Shemang David <davidshemang@gmail.com>
2023-10-24 00:25:28 +0100

    add faq accordion

---
+++ src/app/helpers/faq.data.ts
@@ -0,0 +1,47 @@
+export const FAQData = [
+    {
+      question: "What is VijiPay?",
+      answer: "VijiPay is a secure third-party escrow service designed specifically for users of Jiji Marketplace in Nigeria. It facilitates trusted transactions between buyers and sellers, ensuring seamless and nationwide trade."
+    },
+    {
+        question: "Is VijiPay owned by Jiji?",
+        answer: "VijiPay is currently not owned by Jiji. It is a third-party escrow service designed by a Team of Professional Software Engineers, Digital Marketers and E-commerce experts at Phinx Tech Limited."
+      },
+    {
+      question: "How does VijiPay work?",
+      answer: "Using VijiPay is simple. Sellers list their products, and when a buyer makes a purchase, funds are securely held in escrow. Once the buyer receives the goods or services, the funds are released to the seller."
+    },
+    {
+      question: "Is VijiPay available for sellers and buyers anywhere in Nigeria?",
+      answer: "Yes, VijiPay is accessible to sellers and buyers across Nigeria. Our platform enables transactions regardless of location within the country."
+    },
+    {
+      question: "How do I sign up as a seller on VijiPay?",
+      answer: "To become a seller on VijiPay, simply create an account, provide the necessary information, and start listing your products. Our user-friendly interface makes the process straightforward."
+    },
+    {
+      question: "What payment methods are supported by VijiPay?",
+      answer: "VijiPay supports a variety of payment methods, including bank transfers, mobile money, and popular payment platforms like Flutterwave and Paystack."
+    },
+    {
+      question: "How does VijiPay ensure the security of transactions?",
+      answer: "VijiPay employs industry-standard security measures to safeguard transactions. Our escrow service acts as a neutral third-party, ensuring that both buyers and sellers can trust the process."
+    },
+    {
+      question: "What happens if there is a dispute between a buyer and a seller?",
+      answer: "In the rare event of a dispute, VijiPay has a resolution process in place. Our team will work closely with both parties to reach a fair and satisfactory resolution."
+    },
+    {
+      question: "Are there any fees associated with using VijiPay?",
+      answer: "VijiPay offers competitive pricing with no hidden fees. Our fee structure is transparent, ensuring that both buyers and sellers know what to expect."
+    },
+    {
+      question: "How quickly can I expect to receive my funds as a seller?",
+      answer: "VijiPay strives to release funds promptly once the buyer confirms receipt of the goods or services. This process is designed to be efficient and hassle-free."
+    },
+    {
+      question: "How can I contact VijiPay's customer support team?",
+      answer: "You can reach our dedicated support team through email at support@vijipay.com or by filling out the contact form on our website. We're here to assist you with any questions or concerns."
+    }
+  ];
+  
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { FAQData } from 'src/app/helpers/faq.data';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent],
+  imports: [CommonModule, FooterComponent, NgbAccordionModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  panels: any = FAQData;
 
 
 
