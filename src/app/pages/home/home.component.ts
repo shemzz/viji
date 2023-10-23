1f6171e5eb304222265c68547133377ca5c099a0
Shemang David <davidshemang@gmail.com>
2023-10-23 23:52:10 +0100

    add content to home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+
+
+
+
+playVideo() {
+console.log('play video clicked')
+}
 
 }
