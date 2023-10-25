31bcfcb66978c1d1a4cfd093d4be02efaa499b03
Shemang David <davidshemang@gmail.com>
2023-10-25 19:49:46 +0100

    more updates to the homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { FAQData } from 'src/app/helpers/faq.data';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent, NgbAccordionModule],
+  imports: [CommonModule, FooterComponent, NgbAccordionModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
